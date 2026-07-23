1353797e029636324d539f92b9e630fb5ca8d10a
joelvandiver <joelvandiver@gmail.com>
2026-07-23 17:45:49 -0500

    Resolve clippy ci failure test

--- crates/pulsar-core/src/lib.rs
+++ crates/pulsar-core/src/lib.rs
@@ -4,7 +4,7 @@ pub fn crate_name() -> &'static str {
 
 pub fn add(left: u64, right: u64) -> u64 {
     let test: Result<String, ()> = Ok("abc".to_string());
-    let _ = test.unwrap();
+    let _ = test.expect("This should pass.");
     left + right
 }
 
